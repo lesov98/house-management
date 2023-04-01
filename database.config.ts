9a04a2a82a90978dbadd95ac3cb2271c6186cab6
Miroslav Lesov <miroslav.lesov@novarto.com>
2023-04-02 00:59:48 +0300

    [Fix] env variables not being read runtime

--- database.config.ts
+++ database.config.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv-flow';
+dotenv.config();
+
 export const config = {
   type: process.env.DB_TYPE as 'postgres', //irrelevant as long as you provide the right type in .env
   host: process.env.DB_HOST,
